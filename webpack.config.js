7d250e5746f3e44095d3e19fe72c3390a41f5d04
Rafay <rafaytanzeel@gmail.com>
2017-08-21 14:04:53 -0400

    nsp security check and eslint google added

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,54 @@
-const webpack = require('webpack')
-const path = require('path')
+const path = require('path');
 
-const DIST_DIR = path.join(__dirname, 'dist')
-const SRC_DIR = path.join(__dirname, 'src')
+const DIST_DIR = path.join(__dirname, 'dist');
+const SRC_DIR = path.join(__dirname, 'src');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: path.join(SRC_DIR, 'javascripts','index.js'),
+    index: path.join(SRC_DIR, 'javascripts', 'index.js'),
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(DIST_DIR),
     publicPath: '/',
   },
-  module : {
-    loaders : [{
-        test : /\.js?/,
-        include : path.resolve(SRC_DIR, 'javascripts'),
-        loader : 'babel-loader',
-        query:  {
-          presets: ['react', 'es2015', 'stage-2']
-        }
+  module: {
+    loaders: [{
+      test: /\.js$/, // include .js files
+      enforce: 'pre', // preload the jshint loader
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'jshint-loader',
+        },
+      ],
     }],
-    loaders : [{
+    loaders: [{
+        test: /\.(js|jsx)$/,
+        include: path.resolve(SRC_DIR, 'javascripts'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-2'],
+        },
+    }],
+    loaders: [{
         test: /\.scss$/,
-        include : path.resolve(SRC_DIR, 'styles'),
+        include: path.resolve(SRC_DIR, 'styles'),
         use: ExtractTextPlugin.extract(
           {
             fallback: 'style-loader',
-            use: [ 'css-loader', 'postcss-loader', 'sass-loader' ]
+            use: ['css-loader', 'postcss-loader', 'sass-loader'],
           }
-        )
+        ),
     }],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Portfolio',
       // minify: {collapseWhitespace : true},
-      template: './src/index.pug'
+      template: './src/index.pug',
     }),
-    new ExtractTextPlugin('[name].css')
-  ]
+    new ExtractTextPlugin('[name].css'),
+  ],
 };
