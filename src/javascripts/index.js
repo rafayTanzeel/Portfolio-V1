33f2a2d9b1fa7b533be9297f84e5c690cc9d004b
Rafay <rafaytanzeel@gmail.com>
2017-08-22 07:18:25 -0400

    type animation added

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,17 +5,20 @@ import '../styles/index.scss';
 import '../views/index.pug';
 import './render.js';
 
+import Typist from 'react-typist';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="bg">
-//         import './vert_shader.pug';
-//         import './frag_shader.pug';
-//       </div>
-//     );
-//   }
-// }
 
-// const mountNode = document.getElementById('root');
-// ReactDOM.render(<App />, mountNode);
+class App extends React.Component {
+  render() {
+    return (
+      <Typist startDelay='200' className="typer">
+        Welcome to my Portfolio...<br/><br/>
+        My name is Rafay Tanzeel<br/><br/>
+        I am a Software Engineer
+      </Typist>
+    );
+  }
+}
+
+const mountNode = document.getElementById('root');
+ReactDOM.render(<App />, mountNode);
