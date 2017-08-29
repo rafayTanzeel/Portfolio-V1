0ab0de39ad07d9bb854fb17969156d7162ea3957
Rafay <rafaytanzeel@gmail.com>
2017-08-28 23:55:05 -0400

    init

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -2,30 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../styles/index.scss';
-import '../styles/scrollArrow.scss';
 
 import '../views/index.pug';
-// import './render.js';
-
-import Typist from 'react-typist';
-// import './fullpage';
-
 
 class App extends React.Component {
   render() {
-    const cursor = {
-      show: true,
-      blink: true,
-      element: '|',
-      hideWhenDone: true,
-      hideWhenDoneDelay: 500,
-    };
     return (
-      <Typist startDelay={200} className="typer" cursor={cursor} avgTypingDelay={100} stdTypingDelay={50}>
-        Welcome to my Portfolio...<br/><br/>
-        My name is Rafay Tanzeel<br/><br/>
-        I am a Software Engineer
-      </Typist>
+      <div>
+        <h2>Rafay</h2>
+      </div>
     );
   }
 }
